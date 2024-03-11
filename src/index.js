4cbd6e87ce443e78b4d116476c83a870350acea6
KIRAN UGALE <141510294+kiranugale2o@users.noreply.github.com>
2024-03-11 20:52:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const app=express();
 app.use(require("./router/route"));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
-// app.use(cors({
+app.use(cors({
     
-//         "origin": "http://localhost:3000/",
-//         "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "origin": "http://localhost:3000/",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
         
       
-// }))
+}))
 
 app.get("/",(req,res)=>{
     res.send("home")
