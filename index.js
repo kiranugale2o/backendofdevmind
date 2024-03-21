b79e172efc8eb7a18a04005cecb802abf1de04ce
KIRAN UGALE <141510294+kiranugale2o@users.noreply.github.com>
2024-03-21 08:14:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const app=express();
 app.use(require("./src/router/route"));
 app.use(express.json());
 app.use(express.urlencoded({extended:false})) 
-app.use((req, res, next) => {
-    req.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    req.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+// app.use((req, res, next) => {
+//     req.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     req.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
+
+const corsAllow={
+    origin:"*",
+    method:"PUT,GET,PATCH,DELETE,POST",
+    credentials:true
+}
+
+app.use(cors(corsAllow))
 app.get("/",(req,res)=>{
     res.send("home")
   
